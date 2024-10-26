c8cea55da6f4144255ba00283967bccca163dada
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-10-26 20:20:21 -0300

    faz serviços

--- backend/src/main/java/com/laercio/desafio_fast/workshop/service/ColaboradorService.java
+++ backend/src/main/java/com/laercio/desafio_fast/workshop/service/ColaboradorService.java
@@ -1,5 +1,32 @@
-package com.laercio.desafio_fast.workshop.service;
+package com.fastsolucoes.workshop.service;
 
+import com.fastsolucoes.workshop.model.Colaborador;
+import com.fastsolucoes.workshop.repository.ColaboradorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ColaboradorService {
-    
+
+    @Autowired
+    private ColaboradorRepository colaboradorRepository;
+
+    public List<Colaborador> listarColaboradores() {
+        return colaboradorRepository.findAll();
+    }
+
+    public Colaborador salvarColaborador(Colaborador colaborador) {
+        return colaboradorRepository.save(colaborador);
+    }
+
+    public Colaborador atualizarColaborador(int id, Colaborador colaborador) {
+        colaborador.setId(id);
+        return colaboradorRepository.save(colaborador);
+    }
+
+    public void deletarColaborador(int id) {
+        colaboradorRepository.deleteById(id);
+    }
 }
--- backend/src/main/java/com/laercio/desafio_fast/workshop/service/WorkshopService.java
+++ backend/src/main/java/com/laercio/desafio_fast/workshop/service/WorkshopService.java
@@ -1,5 +1,32 @@
-package com.laercio.desafio_fast.workshop.service;
+package com.fastsolucoes.workshop.service;
 
+import com.fastsolucoes.workshop.model.Workshop;
+import com.fastsolucoes.workshop.repository.WorkshopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class WorkshopService {
-    
+
+    @Autowired
+    private WorkshopRepository workshopRepository;
+
+    public List<Workshop> listarWorkshops() {
+        return workshopRepository.findAll();
+    }
+
+    public Workshop salvarWorkshop(Workshop workshop) {
+        return workshopRepository.save(workshop);
+    }
+
+    public Workshop atualizarWorkshop(int id, Workshop workshop) {
+        workshop.setId(id);
+        return workshopRepository.save(workshop);
+    }
+
+    public void deletarWorkshop(int id) {
+        workshopRepository.deleteById(id);
+    }
 }
