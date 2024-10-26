ce02523445471704b9fa6f843a882dc4cbc128c2
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-10-26 20:37:56 -0300

    descreve as novas classes de view

--- backend/src/main/java/com/laercio/desafio_fast/workshop/view/ColaboradorResponse.java
+++ backend/src/main/java/com/laercio/desafio_fast/workshop/view/ColaboradorResponse.java
@@ -1,5 +1,28 @@
 package com.laercio.desafio_fast.workshop.view;
 
 public class ColaboradorResponse {
-    
+    private int id;
+    private String nome;
+
+    // Construtor, Getters e Setters
+    public ColaboradorResponse(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
--- backend/src/main/java/com/laercio/desafio_fast/workshop/view/WorkshopResponse.java
+++ backend/src/main/java/com/laercio/desafio_fast/workshop/view/WorkshopResponse.java
@@ -1,5 +1,49 @@
 package com.laercio.desafio_fast.workshop.view;
 
 public class WorkshopResponse {
-    
+    private int id;
+    private String nome;
+    private String dataRealizacao;
+    private String descricao;
+
+    // Construtor, Getters e Setters
+    public WorkshopResponse(int id, String nome, String dataRealizacao, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.dataRealizacao = dataRealizacao;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataRealizacao() {
+        return dataRealizacao;
+    }
+
+    public void setDataRealizacao(String dataRealizacao) {
+        this.dataRealizacao = dataRealizacao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
+
