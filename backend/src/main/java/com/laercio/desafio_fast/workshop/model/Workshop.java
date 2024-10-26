0fd5741fa25a7a74094d257cd21985b3570d30dd
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-10-26 20:15:23 -0300

    faz modelagem das entidades

--- backend/src/main/java/com/laercio/desafio_fast/workshop/model/Workshop.java
+++ backend/src/main/java/com/laercio/desafio_fast/workshop/model/Workshop.java
@@ -1,5 +1,20 @@
-package com.laercio.desafio_fast.workshop.model;
+package com.fastsolucoes.workshop.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class Workshop {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+    private LocalDateTime dataRealizacao;
+    private String descricao;
+
+    // Getters e Setters
 }
