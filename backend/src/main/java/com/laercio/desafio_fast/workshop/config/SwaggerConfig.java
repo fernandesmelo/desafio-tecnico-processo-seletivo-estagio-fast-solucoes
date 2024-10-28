aa9a9b1cab08796dc1e538969bfac9bec6983da2
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-10-28 10:26:16 -0300

    atualiza backend

--- backend/src/main/java/com/laercio/desafio_fast/workshop/config/SwaggerConfig.java
+++ backend/src/main/java/com/laercio/desafio_fast/workshop/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.fastsolucoes.workshop.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.laercio.desafio_fast.workshop.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
