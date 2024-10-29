b19c68e5a1b6ce7fac871dbfacdec7a91f459a66
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2024-10-29 10:18:18 -0300

    tira imagem quando fica responsivo

--- backend/src/main/java/com/laercio/desafio_fast/workshop/WorkshopApplication.java
+++ backend/src/main/java/com/laercio/desafio_fast/workshop/WorkshopApplication.java
@@ -1,5 +1,13 @@
 package com.laercio.desafio_fast.workshop;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class WorkshopApplication {
-    
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopApplication.class, args);
+	}
+
 }
